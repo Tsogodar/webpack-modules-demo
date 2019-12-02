05a824031b86a27d3d3d201a6752d8e5bb63b203
Marcin Gryczon <m.gryczon@bankier.pl>
2019-12-02 13:21:55 +0100

    changes

--- module-header/module/main.js
+++ module-header/module/main.js
@@ -1,2 +1,4 @@
 const html = require('./main.html');
 const script = require('../src/script.js');
+const lodash = require('lodash');
+const axios = require('axios');
--- module-header/webpack.config.js
+++ module-header/webpack.config.js
@@ -1,64 +1,65 @@
 const path = require( 'path' );
 const EventHooksPlugin = require( 'event-hooks-webpack-plugin' );
 
-module.exports = {
-	//development | production
-	mode: 'development',
-	entry: [ './module/main.js' ],
-	output: {
-		filename: 'script.js',
-		path: path.resolve( __dirname, 'dist' )
-	},
-	externals: {
-		lodash: '_',
-		axios: 'axios'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['css-loader']
-			},
-			{
-				test: /\.html$/,
-				use: [
-					'file-loader?name=index.[ext]',
-					'extract-loader',
-					'html-loader',
-					{
-						loader: 'posthtml-loader',
-						options: {
-							ident: 'posthtml',
-							plugins: [
-								require('posthtml-modules')(),
-								require( 'posthtml-postcss-modules' )( {
-									from: './src/',
-									generateScopedName: '[hash:base64:5]'
-								} )
-							]
-						}
-					},
-				]
-			}
-		]
-	},
-	plugins: [
-		new EventHooksPlugin( {
-			beforeRun: () =>
-			{
-				const { execFile } = require( 'child_process' );
-				const child = execFile(
-					'node',
-					[ 'build-dependencies-list' ],
-					( error, stdout, stderr ) =>
-					{
-						if ( error )
+module.exports = ( env, options ) =>
+{
+	return {
+		entry: [ './module/main.js' ],
+		output: {
+			filename: '[name].js',
+			path: path.resolve( __dirname, 'dist' )
+		},
+		externals: options. mode === 'production' ? {
+			lodash: '_',
+			axios: 'axios'
+		} : {},
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: [ 'css-loader' ]
+				},
+				{
+					test: /\.html$/,
+					use: [
+						'file-loader?name=index.[ext]',
+						'extract-loader',
+						'html-loader',
 						{
-							throw error;
+							loader: 'posthtml-loader',
+							options: {
+								ident: 'posthtml',
+								plugins: [
+									require( 'posthtml-modules' )(),
+									require( 'posthtml-postcss-modules' )( {
+										from: './src/',
+										generateScopedName: '[hash:base64:5]'
+									} ),
+								]
+							}
+						},
+					]
+				}
+			]
+		},
+		plugins: [
+			new EventHooksPlugin( {
+				beforeRun: () =>
+				{
+					const { execFile } = require( 'child_process' );
+					const child = execFile(
+						'node',
+						[ 'build-dependencies-list' ],
+						( error, stdout, stderr ) =>
+						{
+							if ( error )
+							{
+								throw error;
+							}
 						}
-					}
-				);
-			},
-		} )
-	]
+					);
+				},
+			} )
+		]
+	}
 };
