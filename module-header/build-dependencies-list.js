5b97e7ead2d85110edd06ff164adcc488d102cc6
Marcin Gryczon <m.gryczon@bankier.pl>
2019-12-02 09:27:20 +0100

    return only versions

--- module-header/build-dependencies-list.js
+++ module-header/build-dependencies-list.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-const cdn = 'https://cdn.jsdelivr.net/npm/';
 const packageJson = require('./package.json');
 
 const prodDependencies = Object.entries(packageJson['dependencies']);
@@ -18,8 +17,7 @@ const removeVersionFlag = libVersion => {
 };
 
 prodDependencies.map(lib => {
-	const libCDN = cdn.concat(`${lib[0]}@${removeVersionFlag(lib[1])}/dist/${lib[0]}.min.js`);
-	libs[lib[0]] = libCDN;
+	libs[lib[0]] = removeVersionFlag(lib[1]);
 });
 
 libsHandler.write(JSON.stringify(libs, null, 2));
